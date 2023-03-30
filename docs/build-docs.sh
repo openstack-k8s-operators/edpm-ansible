9d8e31f0a184c4b14d3dea7d1659bf8cb7640e55
Roberto Alfieri <ralfieri@redhat.com>
2023-03-30 16:31:45 +0200

    Added html_docs make target
    * A new script called build-docs.sh in docs directory has been created to
    replace the tox job
    * doc8 settings file docs/doc8.ini has been added
    * function to generate help entries for make command has been improved
    
    Signed-off-by: Roberto Alfieri <ralfieri@redhat.com>

---
+++ docs/build-docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+TEMP_VENV_ENV="/tmp/sphinx"
+DOCS_DIR="./docs"
+
+if [ ! -d ${TEMP_VENV_ENV} ]; then
+    python -m venv ${TEMP_VENV_ENV}
+fi
+
+source ${TEMP_VENV_ENV}/bin/activate
+
+pip install -r requirements.txt -r ${DOCS_DIR}/docs-requirements.txt
+
+doc8 --config ${DOCS_DIR}/doc8.ini ${DOCS_DIR}/source
+sphinx-build -a -E -W -d ${DOCS_DIR}/build/doctrees --keep-going -b html ${DOCS_DIR}/source ${DOCS_DIR}/build/html -T
+
+deactivate
+rm -fr ${TEMP_VENV_ENV}
