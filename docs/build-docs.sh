abc0225f81784ab67a48ddf9b90a044975ae1772
Roberto Alfieri <ralfieri@redhat.com>
2023-03-30 16:47:42 +0200

    install required collection needed by documentation building helpers
    Signed-off-by: Roberto Alfieri <ralfieri@redhat.com>

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -12,6 +12,7 @@ fi
 source ${TEMP_VENV_ENV}/bin/activate
 
 pip install -r requirements.txt -r ${DOCS_DIR}/docs-requirements.txt
+ansible-galaxy install -r requirements.yml
 
 doc8 --config ${DOCS_DIR}/doc8.ini ${DOCS_DIR}/source
 sphinx-build -a -E -W -d ${DOCS_DIR}/build/doctrees --keep-going -b html ${DOCS_DIR}/source ${DOCS_DIR}/build/html -T
