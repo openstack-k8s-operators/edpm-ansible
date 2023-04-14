902de075c6d84ce144dcc16aa89d858d6e97c604
Jiri Podivin <jpodivin@redhat.com>
2023-04-14 14:40:53 +0200

    Local molecule execution in a virtual env
    Testing script report was remade to be more easily readable.
    
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- scripts/test_roles.py
+++ scripts/test_roles.py
@@ -9,12 +9,19 @@ SKIP_LIST = [
 ]
 
 
+def _test_passed(result):
+    if 'CRITICAL' in result['stdout'] or result['stderr'] or result['process'].return_code != 0:
+        return False
+    return True
+
+
 def main():
     role_paths = glob.glob("./roles/*")
     print(f"Found roles: {role_paths}")
     results = {}
 
     for role in role_paths:
+
         role_name = os.path.basename(role)
         if role_name in SKIP_LIST:
             continue
@@ -23,23 +30,27 @@ def main():
             process = sp.Popen(
                 "PY_COLORS=0 molecule test --all",
                 cwd=role, shell=True, stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+            outputs = process.communicate()
             results[role_name] = {
-                'outputs': process.communicate(), 'process': process}
+                'stdout': outputs[0], 'stderr': outputs[1], 'process': process}
+            results[role_name]['passed'] = _test_passed(results[role_name])
         except Exception as ex:
-            results[role_name] = {'outputs': ("", "ERROR"), 'process': ex}
+            results[role_name] = {'outputs': ("", "ERROR"), 'process': ex, 'passed': False}
 
     print("*" * 80)
 
-    for key, val in results.items():
-        test_passed = True
-        if 'CRITICAL' in val['outputs'][0] or val['outputs'][1] or val['process'].return_code != 0:
-            test_passed = False
+    for role, test_result in results.items():
 
-        print(f"{key} - Passed: {test_passed}")
-        if not test_passed:
-            print(f"STDOUT: {val['outputs'][0]}\nSTDERR: {val['outputs'][1]}")
+        print(f"{role} - Passed: {test_result['passed']}")
+        if not test_result['passed']:
+            print(f"STDOUT: {test_result['stdout']}\nSTDERR: {test_result['stderr']}")
+        print("-" * 80)
 
-    print("*" * 80)
+    print("Final report")
+    print("+" * 12)
+    for role, test_result in results.items():
+        print(f"{role} - Passed: {results[role_name]['passed']}")
+    print("-" * 80)
 
 
 if __name__ == '__main__':
